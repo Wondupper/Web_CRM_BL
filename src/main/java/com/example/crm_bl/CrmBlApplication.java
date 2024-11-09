e67701ada212a8ba1d863349ccc8ae2165fbbae6
Wondupper <vanchikwork@gmail.com>
2024-11-09 19:04:00 +0400

    .

---
+++ src/main/java/com/example/crm_bl/CrmBlApplication.java
@@ -0,0 +1,13 @@
+package com.example.crm_bl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmBlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmBlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/crm_bl/CrmBlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crm_bl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmBlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
